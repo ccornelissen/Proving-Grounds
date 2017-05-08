ea512d6d5982ec2a5f8acf8769dc424f98614f18
CODYASUS\Cody <codycornelissen@gmail.com>
2017-05-07 21:00:29 -0700

    Fixed a lot of issues with the new player controller. Player can now aim and shoot correctly in both first and third person. Gun is now correctly alligned with hands in both scenarios. Moved 3rd person player camera to a more ideal position.

--- Source/ProvingGrounds/ProvingGroundsGameMode.cpp
+++ Source/ProvingGrounds/ProvingGroundsGameMode.cpp
@@ -9,7 +9,7 @@ AProvingGroundsGameMode::AProvingGroundsGameMode()
 	: Super()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Blueprints/ThirdPersonCharacter.ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/Player/Blueprints/ThirdPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 	// use our custom HUD class
 	HUDClass = APlayerHUD::StaticClass();
